a193be8d4eb976ec1e5841c4e4792cc0a42a646b
sealocal <local.mat@gmail.com>
2014-04-09 13:55:35 -0700

    re-oganize functionality into a FileRenamer class

--- rename_xml_files_by_content.rb
+++ rename_xml_files_by_content.rb
@@ -2,27 +2,70 @@ require 'nokogiri'
 require 'open-uri'
 require 'fileutils'
 
-# Read in the file and get new file name:
-source_xml_file = ARGV[0]
-puts '**** XML File: ' + source_xml_file
+class FileRenamer
+  attr_reader :source_xml_file, :css_selector, :xml_document,
+      :origin_directory, :export_directory, :new_file_name
 
-origin_directory = File.dirname(source_xml_file)
-puts '**** Origin Directory: ' + origin_directory
+  def initialize(source_xml_file, css_selector)
+    @source_xml_file = source_xml_file
+    @css_selector = css_selector
+  end
 
-doc = Nokogiri::XML(open(source_xml_file))
+  def open_xml_file
+    # Read in the file and get new file name:
+    puts '**** XML File: ' + @source_xml_file
 
+    @origin_directory = File.dirname(@source_xml_file)
+    puts '**** Origin Directory: ' + @origin_directory
+
+    @xml_document = Nokogiri::XML(open(@source_xml_file))
+  end
+
+  def query_xml_file
+    xml_query_by_css = @xml_document.css(@css_selector)
+    puts '**** CSS selector: ' + @css_selector
+
+    @new_file_name = xml_query_by_css.text.strip
+    puts '**** New File Name: ' + @new_file_name
+  end
+
+  #This method should be split into making a directory and exporting a file
+  def export_xml_file
+    # Specify export directory:
+    @export_directory = @origin_directory + '/export_folder'
+    puts '**** Export Directory: ' + @export_directory
+
+    destination_file = @export_directory + '/' + @new_file_name.to_s + '.xml'
+
+    FileUtils.mkdir_p @export_directory
+    FileUtils.copy_file(source_xml_file, destination_file)
+  end
+end
+
+source_data = ARGV[0]
 css_selector = ARGV[1]
-xml_query = doc.css(css_selector)
-puts '**** XML Query by CSS: ' + xml_query.text.strip
 
-new_file_name = xml_query.text.strip
-puts '**** New File Name: ' + new_file_name
+# Example call from console:
+# $ ruby rename_xml_files_by_content.rb example_building_589989.xml 'building Contact alt_building_name'
+if (!File.directory? source_data) && (File.exist? source_data)
+  file_renamer = FileRenamer.new(source_data, css_selector)
+  file_renamer.open_xml_file
+  file_renamer.query_xml_file
+  file_renamer.export_xml_file
+end
 
-# Specify export directory:
-export_directory = origin_directory + '/export_folder'
-puts '**** Export Directory: ' + export_directory
+if File.directory? source_data
+  Dir.chdir(source_data) do
+    all_regular_files_in_directory = Dir.glob('*.*')
+    all_regular_files_in_directory.each do |file_name|
+      file_renamer = FileRenamer.new(file_name, css_selector)
+      file_renamer.open_xml_file
+      file_renamer.query_xml_file
+      file_renamer.export_xml_file
+    end
+  end
+end
 
-destination_file = export_directory + '/' + new_file_name.to_s + '.xml'
 
-FileUtils.mkdir_p export_directory
-FileUtils.copy_file(source_xml_file, destination_file)
+# Checkout the public repo at:
+# https://github.com/sealocal/rename-xml-files-by-content
